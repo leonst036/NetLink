c95e62551bc74758e1d22a31a41eef0ebd734a49
Leon <leonst036@gmail.com>
2026-08-13 12:58:00 +0200

    feat: implement dark theme using MUI ThemeProvider and CssBaseline in root component

--- applications/net-graph/frontend/src/main.tsx
+++ applications/net-graph/frontend/src/main.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
 import App from './App';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
